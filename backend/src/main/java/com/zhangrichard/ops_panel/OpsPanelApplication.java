82944ab5db74454ec96a101136d924ad8352d931
zhangyj <richard.yujia.zhang@gmail.com>
2025-10-31 11:05:24 +0800

    feat: 后端项目spring boot initializr初始化

---
+++ backend/src/main/java/com/zhangrichard/ops_panel/OpsPanelApplication.java
@@ -0,0 +1,13 @@
+package com.zhangrichard.ops_panel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpsPanelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpsPanelApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/zhangrichard/ops_panel/OpsPanelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhangrichard.ops_panel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpsPanelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
