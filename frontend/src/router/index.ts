278f5f457992e78c9371c435e06c9283d08fab05
zhangyj <richard.yujia.zhang@gmail.com>
2025-10-31 15:20:37 +0800

    feat: router调整，layout框架

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,8 +1,28 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
+import MainLayout from '@/layouts/MainLayout.vue'
+import TestView from '../views/TestView.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  history: createWebHashHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: TestView,
+    },
+    {
+      path: '/main',
+      name: 'main-layout',
+      component: MainLayout,
+      children: [
+        {
+          path: '/main/test',
+          name: 'test-view',
+          component: () => import('../views/TestView.vue'),
+        },
+      ],
+    },
+  ],
 })
 
 export default router
