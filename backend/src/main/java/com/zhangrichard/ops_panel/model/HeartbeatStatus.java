41317f21958227d79d100165041225ef83237ca8
ZhangYJ <richard.yujia.zhang@gmail.com>
2025-11-08 16:38:47 +0800

    feat: 接受MQTT心跳

---
+++ backend/src/main/java/com/zhangrichard/ops_panel/model/HeartbeatStatus.java
@@ -0,0 +1,35 @@
+package com.zhangrichard.ops_panel.model;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class HeartbeatStatus {
+
+    private Integer id;
+
+    private Double cpuUsage;
+
+    private Double ramUsage;
+
+    private List<DiskStatus> disks;
+
+    private List<ServiceStatus> services;
+
+    @Data
+    public static class DiskStatus {
+
+        private Integer id;
+
+        private Double diskUsage;
+    }
+
+    @Data
+    public static class ServiceStatus {
+
+        private Integer id;
+
+        private Boolean online;
+    }
+}
